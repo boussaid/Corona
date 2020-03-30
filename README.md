5146c414032341e00505a7a7f3c61e1fc14ed12a
Boussaid Mustafa <bousali@gmail.com>
2020-03-30 03:49:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+ # XenForo Addon: Coronavirus Widget Statistics
+ 
+ > Addon by: Boussaid Mustafa
+ 
+ >Copyright 2020
+ 
+
+*******************************************/
+
+XenForo 2.1.x
---
+++ Widget/Corona.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Corona\Widget;
+
+use XF\Widget\AbstractWidget;
+
+class Corona extends AbstractWidget {
+
+protected $defaultOptions = [
+		'contry' => 'DZ'
+	];
+
+
+public function render()
+{
+  $options = $this->options;
+	$contry = $options['contry'];
+		
+		$all = file_get_contents("https://corona.lmao.ninja/all");
+		$dz = file_get_contents("https://corona.lmao.ninja/countries/".$contry);
+		$xall = json_decode($all, true);
+		$xcontry = json_decode($dz, true);		
+		
+		 $viewParams = [
+		 'all'    => $xall,
+		 'contrys'    => $xcontry
+		 ];
+		return $this->renderer('widget_Corona', $viewParams); 	       
+}
+public function verifyOptions(\XF\Http\Request $request, array &$options, &$error = null)
+	{
+		$options = $request->filter([
+			'contry' => 'str'
+		]);
+		return true;
+	}
+
+}
